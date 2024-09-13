aafaed6655c8e5190260ecafb7d7c5cd0b1f7e1f
zrao4021 <zrao4021@gmail.com>
2024-09-13 17:27:25 +0500

    my assignment

---
+++ app.js
@@ -0,0 +1,149 @@
+// question no 1....
+var city = prompt("enter where do you live in");
+
+if(city === "karachi" ){
+    alert("welcome to city of lights");
+}
+else{
+    alert("welcome you live in"  + city);
+}
+// question no 2.....
+var gender = prompt("enter your gender");
+if( gender === "male"){
+     alert("Good morning sir")
+}
+else{
+    alert("Good morning Ma'am")
+}
+
+// question no 3....
+var color = prompt("enter signal color");
+if( color == "red" ){
+      alert("Must Stop");
+}
+else if (color == "yellow"){
+    alert("Ready to move")
+}
+
+else if (color == "green"){
+    alert("Move now")
+}
+else {
+    alert("Incorrect");
+}
+// question no 4....
+  var fuel = +prompt("enter your remaining fuel in car");
+  if( fuel < 0.25){
+       alert("please refill the fuel in your car");
+  }
+  else {
+    alert("set");
+  }
+// question no 5....
+   var a =4;
+   if(++a === 5){
+    alert("given condition for variable a is true");
+   }
+    var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+}
+ var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}
+ var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}
+ if (true){
+alert("True");
+}
+if (false){
+alert("False");
+}
+
+ if("car" < "cat"){
+alert("car is smaller than cat");
+}
+// question no 7....a
+var number = +prompt("set");
+if(number == 2){
+    alert("!correct answer");
+}
+if(number == 1){
+    alert("Close enough to the correct answer");
+}
+else{
+    alert("wong answer");
+}
+// question no 7....b
+var num = prompt("enter a Number");
+if(num % 3 === 3 ){
+  alert(number + "is divided by three");
+}
+else if(num % 3 === 6){
+    alert(number + "is divided by three");
+}
+else if(num % 3 === 9 ){
+    alert(number + "is divided by three");
+}
+else if(num % 3 === 12){
+    alert(number + "is divided by three");
+}
+else if(num % 3 === 15){
+    alert(number + "is divided by three")
+}
+else if(num % 3 === 18){
+    alert(number + "is divided by three")
+}
+else if(num % 3 === 21 ){
+    alert(number + "is divided by three")
+}
+else if(num % 3 === 24){
+    alert(number + "is divided by three")
+}
+else if(num % 3 === 27){
+    alert(number + "is divided by three")
+}
+else if(num % 3 === 30){
+    alert(number + "is divided by three")
+}
+else{
+    alert("the num is divided by three")
+}
+// question no 10....
+var temperature = +prompt("enter temperature")
+
+if(temperature > 40) {
+     alert("its too hot outside");
+}
+else if(temperature > 30) {
+    alert("the weather today is normal");
+}
+else if(temperature > 20) {
+    alert("todays weather is cool");
+}
+else if(temperature > 10) {
+    alert("OMG today weather is so cool ");
+} else{
+    alert("incorrect");
+}
+// question no 11.. ..
+// is on indexfile because of not working in jawa file.
+// question 9 also on index ...
+// question 6...starts.
+
+
+
